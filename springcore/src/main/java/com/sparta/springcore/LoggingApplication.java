d10ede80550547c54a8715fde8d84c8c5f75d99b
ljh468 <ljh468@naver.com>
2022-01-07 15:35:20 +0900

    mvc test

--- springcore/src/main/java/com/sparta/springcore/LoggingApplication.java
+++ springcore/src/main/java/com/sparta/springcore/LoggingApplication.java
@@ -1,24 +1,24 @@
-package com.sparta.springcore;
-
-import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@Slf4j
-@SpringBootApplication
-public class LoggingApplication {
-//    private static final Logger log = LogManager.getLogger(LoggingApplication.class);
-
-    public static void main(String[] args) {
-        System.out.println(" = " + "log4j2");
-        log.trace("Hi I'm {} log", "TRACE");
-        log.debug("Hi I'm {} log", "DEBUG");
-        log.info("Hi I'm {} log", "INFO");
-        log.warn("Hi I'm {} log", "WARN");
-        log.error("Hi I'm {} log", "ERROR");
-
-    }
-
-}
+//package com.sparta.springcore;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@Slf4j
+//@SpringBootApplication
+//public class LoggingApplication {
+////    private static final Logger log = LogManager.getLogger(LoggingApplication.class);
+//
+//    public static void main(String[] args) {
+//        System.out.println(" = " + "log4j2");
+//        log.trace("Hi I'm {} log", "TRACE");
+//        log.debug("Hi I'm {} log", "DEBUG");
+//        log.info("Hi I'm {} log", "INFO");
+//        log.warn("Hi I'm {} log", "WARN");
+//        log.error("Hi I'm {} log", "ERROR");
+//
+//    }
+//
+//}
